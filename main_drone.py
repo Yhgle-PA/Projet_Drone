41727afd216bf98d5e0539a4a05ea38f794b8c5b
Rolithh <alexandre.presa@hotmail.fr>
2020-09-18 08:16:14 +0200

    Ajout de zones

--- main_drone.py
+++ main_drone.py
@@ -1,13 +1,17 @@
 from class_launching import LaunchingBase
+from random_destination import RandomDestination
 from random import randint, random
 
 
 def main_drone(x_base, y_base, x_max, y_max, nb_drone, speed_drone, high_drone, dt, delivery_time, time_wait_base, time_tot_simu):
     base = LaunchingBase(nb_drone, x_base, y_base, speed_drone, high_drone, x_max, y_max, delivery_time, dt, time_wait_base)
+    destinations = RandomDestination(x_max, y_max)
 
     for i in range(time_tot_simu//dt):
         if random() < 1:
-            base.new_order(randint(0, x_max), randint(0, y_max))
+            #base.new_order(randint(0, x_max), randint(0, y_max))
+            x, y = destinations.random()
+            base.new_order(x, y)
         base.launch_order()
         base.move_all()
         base.check_arrived_order()
---
+++ random_destination.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class RandomDestination:
+    
+    def __init__(self, x_max, y_max):
+        self.density = np.load("density_map.npy")
+        self.shape = self.density.shape
+        self.density = self.density.flatten()
+        self.density /= np.sum(self.density)
+        
+        self.xscale = x_max/self.shape[1]
+        self.yscale = y_max/self.shape[0]
+        
+        
+    def random(self):
+        r = np.random.choice(self.density.shape[0], p=self.density)
+        y, x = np.unravel_index(r, self.shape)
+        return x*self.xscale, (self.shape[0]-y)*self.yscale
+        
+        
