984081bc9428adb8439c0861b4276c6a9fc34487
Yhgle <pierre.allegre@laposte.net>
2020-09-18 08:28:10 +0200

    Error fix merge

--- main_drone.py
+++ main_drone.py
@@ -1,16 +1,15 @@
 from class_launching import LaunchingBase
 from random_destination import RandomDestination
-from random import randint, random
+from random import random
 import json
 
 
-def main_drone(x_base, y_base, x_max, y_max, nb_drone, speed_drone, high_drone, dt, delivery_time, time_wait_base, time_tot_simu):
-    base = LaunchingBase(nb_drone, x_base, y_base, speed_drone, high_drone, x_max, y_max, delivery_time, dt, time_wait_base)
-    destinations = RandomDestination(x_max, y_max)
+def main_drone(dict_setup):
+    base = LaunchingBase(dict_setup)
+    destinations = RandomDestination(dict_setup["x_max"], dict_setup["y_max"])
 
     for i in range(dict_setup["time_tot_simu"]//dict_setup["dt"]):
         if random() < 1:
-            #base.new_order(randint(0, x_max), randint(0, y_max))
             x, y = destinations.random()
             base.new_order(x, y)
         base.launch_order()
